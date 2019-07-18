717deae088087a9f49841064bacd7a54c15782b7
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-18 17:23:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
 
+if word.match?(/\A[aeiouAEIOU]/) 
+  return true
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/(\A+un)(\z+ing)/)
 end
 
 def words_five_letters_long(text)
